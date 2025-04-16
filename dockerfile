9a01452331e75954467243868f2436c8a57eb7c2
Omniladder <dustintobrien@gmail.com>
2025-04-16 00:05:12 -0400

    Updating Setup

--- dockerfile
+++ dockerfile
@@ -1,1 +1,6 @@
+FROM ubuntu:20.04
 
+RUN apt update && apt install -y git
+RUN git clone https://github.com/Omniladder/Dustin_Lambda_Check.git
+WORKDIR Dustin_Lambda_Check
+RUN sh setup.sh
--- setup.sh
+++ setup.sh
@@ -3,21 +3,25 @@
 
 cd CLI
 
-curl --proto '=https' --tlsv1.2 -sSf https://get-ghcup.haskell.org | sh
+#curl --proto '=https' --tlsv1.2 -sSf https://get-ghcup.haskell.org | sh
+
+sudo apt-get update
+sudo apt-get install cabal-install
+
+
+sudo apt install python3.12
+
+pip install -r ../requirements.txt
 
 cabal build
 
-cabal run
+#cabal run
 
-echo " alias = \" ~/lambda-check/CLI/dist-newstyle/build/*/ghc-8.8.4/Lambda-Check-0.1.0.0/x/Lambda-Check/build/Lambda-Check \" " >> ~/.bashrc
+#echo " alias = \" ~/lambda-check/CLI/dist-newstyle/build/*/ghc-8.8.4/Lambda-Check-0.1.0.0/x/Lambda-Check/build/Lambda-Check \" " >> ~/.bashrc
 
-pip install sqlite3
-pip install fastapi
 
-source ~/.bashrc
 
-cabal install text
-cabal install http-conduit
-cabal install bytestring
-cabal install ansi-terminal
+#source ~/.bashrc
+
+
 
